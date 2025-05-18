4ef812893868d6f3485142a7f1a4c71279f565a7
Alexandru Armas <alexandru.armas22@gmail.com>
2025-05-18 03:45:34 +0300

    Fix Clerk middleware implementation for version 5.7.5

--- middleware.ts
+++ middleware.ts
@@ -1,26 +1,8 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { clerkMiddleware } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-// This example protects all routes including api/trpc routes
-// Please edit this to allow other routes to be public as needed.
-// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
-export default authMiddleware({
-  // Routes that can be accessed while signed out
-  publicRoutes: [
-    "/",
-    "/sign-in*",
-    "/sign-up*",
-    "/api/check-stream-config", // Allow checking config without auth
-  ],
-  // Routes that can always be accessed, and have
-  // no authentication information
-  ignoredRoutes: [
-    "/_next/static/(.*)",
-    "/favicon.ico",
-  ],
-});
+export default clerkMiddleware();
 
 export const config = {
-  // Protects all routes, including api/trpc.
-  // See https://clerk.com/docs/references/nextjs/auth-middleware for more
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
 };
